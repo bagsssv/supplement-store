74dd88edebbc6c53fa9e9156b442d7a62794d926
bagsssv <semenkakitiga@gmail.com>
2026-09-04 17:20:36 +0700

    fix typo name

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import Navbar from './components/Layout/Navbar';
+import Navbar from './components/layout/Navbar';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
 import Cart from './pages/Cart';
 import Member from './pages/Member';
 import Checkout from './pages/Checkout';
 import { CartProvider } from './contexts/CartContext';
-import Footer from './components/Layout/Footer';
+import Footer from './components/layout/Footer';
 
 function App() {
   return (
